d595606029d196ab7cb3d5a5268287a73ad96573
nayab jahan <nayabzafir@gmail.com>
2022-08-11 21:18:37 +0530

    Added:Creation Of Contact

--- src/main/java/com/blz/addressBook/AddressBookMain.java
+++ src/main/java/com/blz/addressBook/AddressBookMain.java
@@ -3,5 +3,6 @@ package com.blz.addressBook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("welcome to Address Book ");
+        Contacts contact = new Contacts();
     }
 }
---
+++ src/main/java/com/blz/addressBook/Contacts.java
@@ -0,0 +1,91 @@
+package com.blz.addressBook;
+
+public class Contacts {
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNo;
+    private String eMail;
+
+    public Contacts() {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.eMail = eMail;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", eMail='" + eMail + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+}
+
